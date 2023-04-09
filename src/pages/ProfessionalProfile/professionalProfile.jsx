974c14a95ac2c2d909433efff6a95e46c725591c
Agenor Torres <agenorpoker@gmail.com>
2023-04-09 16:57:58 -0300

    concluido a primeira parte da página de exibição do profissional

--- src/pages/ProfessionalProfile/professionalProfile.jsx
+++ src/pages/ProfessionalProfile/professionalProfile.jsx
@@ -1,44 +1,68 @@
 import { Link } from 'react-router-dom';
 import './professionalProfile.css';
+import ImgTeste from '../../assets/fotoTeste.jpg'
+import IconLeft from '../../assets/arrow-left.svg'
 
 function ProfessionalProfile() {
     return (
         <div className='container-professionalProfile'>
-            <Link to='/categories'>Voltar</Link>
-
+            <div className='container-professionalProfile-back'>
+                <img
+                    style={{ width: '1rem' }}
+                    src={IconLeft} alt="seta para esquerda" />
+                <Link to='/categories'>
+                    Voltar
+                </Link>
+            </div>
             <div>
                 <div className='container-professionalProfile-description'>
-                    <div
-                        style={{ display: 'flex', flexDirection: 'column', gap: '2rem' }}>
+                    <div className='container-professionalProfile-description-information'>
                         <div
                             style={{ display: 'flex', gap: '2rem' }}>
-
                             <p>Data</p>
-                            <p>Plano</p>
+                            <p>Plus</p>
                         </div>
                         <div style={{ display: 'flex', gap: '2rem' }}>
-                            <p>Idade: { }</p>
-                            <p>Categoria: { }</p>
-                            <p>Cidade: { }</p>
+                            <p>22</p>
+                            <p>Acompanhante</p>
+                            <p>Recife</p>
                         </div>
                     </div>
-                    <h1>Título feito pelo usuário</h1>
-                    <h3>Descrição feito pelo usuário</h3>
+                    <div className='container-professionalProfile-description-information-titleDescription'>
+                        <h1>Nome do Profissional</h1>
+                        <p>Descrição feito pelo usuário Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio totam alias laboriosam iure eligendi magni iste perferendis. Sapiente quis, recusandae nemo atque, natus molestias, ea modi ad quia voluptate veniam!</p>
+                    </div>
 
-                    <div className='container-professionalProfile-image'>
-                        <img src="https://a-static.mlcdn.com.br/800x560/boneca-mulher-maravilha-com-laco-wonder-woman/abrakadabra/549/04340fcc9f8e0a666dc06fff0e8b2cd7.jpeg" alt="boneco da mulher maravilha" />
-                        <img src="https://a-static.mlcdn.com.br/800x560/boneca-mulher-maravilha-com-laco-wonder-woman/abrakadabra/549/04340fcc9f8e0a666dc06fff0e8b2cd7.jpeg" alt="boneco da mulher maravilha" />
-                        <img src="https://a-static.mlcdn.com.br/800x560/boneca-mulher-maravilha-com-laco-wonder-woman/abrakadabra/549/04340fcc9f8e0a666dc06fff0e8b2cd7.jpeg" alt="boneco da mulher maravilha" />
-                        <img src="https://a-static.mlcdn.com.br/800x560/boneca-mulher-maravilha-com-laco-wonder-woman/abrakadabra/549/04340fcc9f8e0a666dc06fff0e8b2cd7.jpeg" alt="boneco da mulher maravilha" />
+                    <div
+                        className='container-professionalProfile-image'>
+                        <img
+                            src={ImgTeste}
+                            alt="mulher sorrindo" />
+                        <img src={ImgTeste}
+                            alt="mulher sorrindo" />
+                        <img src={ImgTeste}
+                            alt="mulher sorrindo" />
+                        <img src={ImgTeste}
+                            alt="mulher sorrindo" />
                     </div>
                 </div>
             </div>
-            <h2>Entre em contato comigo</h2>
-            <div className='container-professionalProfile-button'>
-                <button>TELEFONE</button>
-                <button>WHATSAPP</button>
+            <div className='container-professionalProfile-contact'>
+                <h4>
+                    Faça já sua reserva pelo site e tenha a garantia que o profissional é verificado ou seu dinheiro de volta
+                </h4>
+                <div className='container-professionalProfile-contact-button'>
+                    <button>RESERVA</button>
+                </div>
 
             </div>
+            <footer>
+                <a href="">Termos e Condições</a>
+                <a href="">Politica de Privacidade</a>
+                <a href="">Fale Conosco</a>
+                <a id='end' href="">Promover seus Anúncios</a>
+
+            </footer>
         </div>
     )
 }
